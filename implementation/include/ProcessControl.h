8a1f04a8a65207b721c75964366973c1a747e19a
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-03-05 23:25:02 +0530

    Commit process control
    A logic which includes implementation of killing specific process and changing its priority.
    
    Co-Authored-By: Sethukumar_M <140262137+SETHUKUMAR1709@users.noreply.github.com>
    Co-Authored-By: Harsh Chaudhary <149260243+Harshjaa8@users.noreply.github.com>

---
+++ implementation/include/ProcessControl.h
@@ -0,0 +1,14 @@
+#ifndef PROCESSCONTROL_H
+#define PROCESSCONTROL_H
+
+namespace ProcessControl {
+    bool killProcess(int pid) {
+        return true;
+    }
+
+    bool changeProcessPriority(int pid, int priority) {
+        return true;
+    }
+}
+
+#endif // PROCESSCONTROL_H
